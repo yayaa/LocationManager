c20de28da8f20caec8af0110c76a638ccc1518d9
victor-denisenko <victor.dens@mail.ru>
2020-07-29 10:13:07 +0300

    Delete unused import

--- library/src/test/java/com/yayandroid/locationmanager/providers/locationprovider/DispatcherLocationProviderTest.java
+++ library/src/test/java/com/yayandroid/locationmanager/providers/locationprovider/DispatcherLocationProviderTest.java
@@ -24,7 +24,6 @@ import org.mockito.MockitoAnnotations;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
