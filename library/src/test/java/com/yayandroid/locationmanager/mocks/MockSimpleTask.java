ff02f75d5462ea5ad56c7cbec08279966c4cf458
victor-denisenko <victor.dens@mail.ru>
2020-07-31 14:44:00 +0300

    Update MockSimpleTask, delete unused code

--- library/src/test/java/com/yayandroid/locationmanager/mocks/MockSimpleTask.java
+++ library/src/test/java/com/yayandroid/locationmanager/mocks/MockSimpleTask.java
@@ -5,8 +5,6 @@ import android.app.Activity;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.google.android.gms.common.internal.Preconditions;
-import com.google.android.gms.tasks.OnCanceledListener;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -31,22 +29,22 @@ public class MockSimpleTask<TResult> extends Task<TResult> {
         this.error = error;
     }
 
-    public MockSimpleTask() {
-
-    }
-
+    @Override
     public boolean isComplete() {
         return true;
     }
 
+    @Override
     public boolean isCanceled() {
         return false;
     }
 
+    @Override
     public boolean isSuccessful() {
         return this.error == null;
     }
 
+    @Override
     public TResult getResult() {
         if (this.error != null) {
             throw new RuntimeExecutionException(this.error);
@@ -55,9 +53,10 @@ public class MockSimpleTask<TResult> extends Task<TResult> {
         }
     }
 
-    public <X extends Throwable> TResult getResult(@NonNull Class<X> var1) throws X {
-        if (var1.isInstance(this.error)) {
-            throw var1.cast(this.error);
+    @Override
+    public <X extends Throwable> TResult getResult(@NonNull Class<X> throwableClass) throws X {
+        if (throwableClass.isInstance(this.error)) {
+            throw throwableClass.cast(this.error);
         } else if (this.error != null) {
             throw new RuntimeExecutionException(this.error);
         } else {
@@ -66,139 +65,79 @@ public class MockSimpleTask<TResult> extends Task<TResult> {
     }
 
     @Nullable
+    @Override
     public Exception getException() {
         return this.error;
     }
 
     @NonNull
-    public Task<TResult> addOnSuccessListener(@NonNull OnSuccessListener<? super TResult> var1) {
+    @Override
+    public Task<TResult> addOnSuccessListener(@NonNull OnSuccessListener<? super TResult> listener) {
         if (isSuccessful()) {
-            var1.onSuccess(result);
+            listener.onSuccess(result);
         }
 
         return this;
     }
 
     @NonNull
-    public Task<TResult> addOnSuccessListener(@NonNull Executor var1, @NonNull OnSuccessListener<? super TResult> var2) {
+    @Override
+    public Task<TResult> addOnSuccessListener(@NonNull Executor executor, @NonNull OnSuccessListener<? super TResult> listener) {
         if (isSuccessful()) {
-            var2.onSuccess(result);
+            listener.onSuccess(result);
         }
 
         return this;
     }
 
     @NonNull
-    public Task<TResult> addOnSuccessListener(@NonNull Activity var1, @NonNull OnSuccessListener<? super TResult> var2) {
+    @Override
+    public Task<TResult> addOnSuccessListener(@NonNull Activity activity, @NonNull OnSuccessListener<? super TResult> listener) {
         if (isSuccessful()) {
-            var2.onSuccess(result);
+            listener.onSuccess(result);
         }
 
         return this;
     }
 
     @NonNull
-    public Task<TResult> addOnFailureListener(@NonNull OnFailureListener var1) {
+    @Override
+    public Task<TResult> addOnFailureListener(@NonNull OnFailureListener listener) {
         if (error != null && !isSuccessful()) {
-            var1.onFailure(error);
+            listener.onFailure(error);
         }
 
         return this;
     }
 
     @NonNull
-    public Task<TResult> addOnFailureListener(@NonNull Executor var1, @NonNull OnFailureListener var2) {
+    @Override
+    public Task<TResult> addOnFailureListener(@NonNull Executor executor, @NonNull OnFailureListener listener) {
         if (error != null && !isSuccessful()) {
-            var2.onFailure(error);
+            listener.onFailure(error);
         }
 
         return this;
     }
 
     @NonNull
-    public Task<TResult> addOnFailureListener(@NonNull Activity var1, @NonNull OnFailureListener var2) {
+    @Override
+    public Task<TResult> addOnFailureListener(@NonNull Activity activity, @NonNull OnFailureListener listener) {
         if (error != null && !isSuccessful()) {
-            var2.onFailure(error);
+            listener.onFailure(error);
         }
 
         return this;
     }
 
     @NonNull
-    public Task<TResult> addOnCompleteListener(@NonNull OnCompleteListener<TResult> var1) {
+    @Override
+    public Task<TResult> addOnCompleteListener(@NonNull OnCompleteListener<TResult> listener) {
         if (isComplete()) {
-            var1.onComplete(this);
+            listener.onComplete(this);
         }
 
         return this;
-    }
-
-    @NonNull
-    public Task<TResult> addOnCompleteListener(@NonNull Executor var1, @NonNull OnCompleteListener<TResult> var2) {
-        if (isComplete()) {
-            var2.onComplete(this);
-        }
-
-        return this;
-    }
-
-    @NonNull
-    public Task<TResult> addOnCompleteListener(@NonNull Activity var1, @NonNull OnCompleteListener<TResult> var2) {
-        if (isComplete()) {
-            var2.onComplete(this);
-        }
-
-        return this;
-    }
-
-    @NonNull
-    public Task<TResult> addOnCanceledListener(@NonNull OnCanceledListener var1) {
-        if (isCanceled()) {
-            var1.onCanceled();
-        }
-
-        return this;
-    }
-
-    @NonNull
-    public Task<TResult> addOnCanceledListener(@NonNull Executor var1, @NonNull OnCanceledListener var2) {
-        if (isCanceled()) {
-            var2.onCanceled();
-        }
-
-        return this;
-    }
-
-    @NonNull
-    public Task<TResult> addOnCanceledListener(@NonNull Activity var1, @NonNull OnCanceledListener var2) {
-        if (isCanceled()) {
-            var2.onCanceled();
-        }
-
-        return this;
-    }
-
-    public void setResult(@Nullable TResult var1) {
-        this.result = var1;
-    }
-
-    public boolean trySetResult(TResult var1) {
-        this.result = var1;
-
-        return true;
-    }
-
-    public void setException(@NonNull Exception var1) {
-        Preconditions.checkNotNull(var1, "Exception must not be null");
-
-        this.error = var1;
-    }
-
-    public boolean trySetException(@NonNull Exception var1) {
-        Preconditions.checkNotNull(var1, "Exception must not be null");
-        this.error = var1;
-
-        return true;
     }
 
 }
